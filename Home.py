a51c39684eb3f8ee781c8b0cdd0ec62054acb3bf
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-04-03 21:20:51 +0530

    Increased Redability

--- Home.py
+++ Home.py
@@ -18,42 +18,43 @@ if st.button(label='Submit'):
         
         # Display Films
         if character_info[1]:
-            st.write(f"**Films:**")
-            st.write("  - " + "\n  - ".join(character_info[1].split(',')))
+            with st.expander("Films"):
+                st.write("  - " + "\n  - ".join(character_info[1].split(',')))
         
         # Display Short Films
         if character_info[2]:
-            st.write(f"**Short Films:**")
-            st.write("  - " + "\n  - ".join(character_info[2].split(',')))
+            with st.expander("Short Films"):
+                st.write("  - " + "\n  - ".join(character_info[2].split(',')))
         
         # Display TV Shows
         if character_info[3]:
-            st.write(f"**TV Shows:**")
-            st.write("  - " + "\n  - ".join(character_info[3].split(',')))
+            with st.expander("TV Shows"):
+                st.write("  - " + "\n  - ".join(character_info[3].split(',')))
         
         # Display Video Games
         if character_info[4]:
-            st.write(f"**Video Games:**")
-            st.write("  - " + "\n  - ".join(character_info[4].split(',')))
+            with st.expander("Video Games"):
+                st.write("  - " + "\n  - ".join(character_info[4].split(',')))
         
         # Display Park Attractions
         if character_info[5]:
-            st.write(f"**Park Attractions:**")
-            st.write("  - " + "\n  - ".join(character_info[5].split(',')))
+            with st.expander("Park Attractions"):
+                st.write("  - " + "\n  - ".join(character_info[5].split(',')))
         
         # Display Allies
         if character_info[6]:
-            st.write(f"**Allies:**")
-            st.write("  - " + "\n  - ".join(character_info[6].split(',')))
+            with st.expander("Allies"):
+                st.write("  - " + "\n  - ".join(character_info[6].split(',')))
         
         # Display Enemies
         if character_info[7]:
-            st.write(f"**Enemies:**")
-            st.write("  - " + "\n  - ".join(character_info[7].split(',')))
+            with st.expander("Enemies"):
+                st.write("  - " + "\n  - ".join(character_info[7].split(',')))
         
         # Display Source URL
         if character_info[8]:
-            st.markdown(f"<a href='{character_info[8]}' target='_blank'>Read more</a>", unsafe_allow_html=True)
+            with st.expander("Source"):
+                st.markdown(f"<a href='{character_info[8]}' target='_blank'>Read more</a>", unsafe_allow_html=True)
 
     else:
         st.write("Character not found.")
